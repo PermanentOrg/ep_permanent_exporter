1a8fdbfc1d28418f1c8257ee8be731e5b99b2bee
Jason Owen <jason@jasonaowen.net>
2021-03-04 13:09:41 -0800

    Set up example document ready hook
    Add the initial outline of a hook that is called once the page is
    loaded, to verify that client hooks are being loaded correctly.

---
+++ src/client/document-ready.ts
@@ -0,0 +1,6 @@
+const documentReady = (hookName: string) => {
+  // eslint-disable-next-line no-console
+  console.log('ep_permanent_exporter hook called:', hookName);
+};
+
+export { documentReady };
