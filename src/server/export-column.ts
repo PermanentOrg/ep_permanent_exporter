147755a144952002cc9c6d0defa59fdf29e602b3
Jason Owen <jason@jasonaowen.net>
2021-03-11 10:58:16 -0800

    Use .html extension for templates without JS
    Etherpad Lite uses EJS[1] to handle templating, and so the documentation
    (reasonably) suggests naming your templates in .ejs. However, for simple
    templates that contain no dynamic content, giving them an .html
    extension communicates their simplicity both to developers and to
    editors, which can then do HTML syntax highlighting.
    
    Rename our single template.
    
    [1] https://www.npmjs.com/package/ejs

--- src/server/export-column.ts
+++ src/server/export-column.ts
@@ -1,7 +1,7 @@
 import eejs from 'ep_etherpad-lite/node/eejs';
 
 const eejsBlock_exportColumn = (hookName: string, args: { content: string }) => {
-  args.content += eejs.require('ep_permanent_exporter/templates/exportColumn.ejs');
+  args.content += eejs.require('ep_permanent_exporter/templates/exportColumn.html');
 };
 
 export { eejsBlock_exportColumn };
