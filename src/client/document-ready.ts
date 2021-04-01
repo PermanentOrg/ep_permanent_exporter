c2cadd53baa1b4aad3f37c23f573a7e391b97e24
Jason Owen <jason@jasonaowen.net>
2021-04-01 12:14:01 -0700

    Wire up jQuery for use in prototype frontend form
    Set up an initial prototype of the form users will use to turn on
    syncing to their Permanent archive. In order for the form to correctly
    send data to the Etherpad backend, and not do a full page load, we need
    to attach an event handler to the form. Etherpad provides jQuery, which
    we can use in the document ready hook to look up our element and attach
    our handler.

--- src/client/document-ready.ts
+++ src/client/document-ready.ts
@@ -1,6 +1,32 @@
+import type JQuery from 'jquery';
+
+const padUrl = () => (
+  window.location.href.replace(window.location.search, '')
+);
+
 const documentReady = (hookName: string) => {
   // eslint-disable-next-line no-console
   console.log('ep_permanent_exporter hook called:', hookName);
+
+  $('#ep_permanent_exporter-export-form').submit((event: JQuery.SubmitEvent) => {
+    event.preventDefault();
+
+    // eslint-disable-next-line no-console
+    console.log('ep_permanent_exporter form submitted:', event);
+
+    $.ajax({
+      method: 'PUT',
+      url: `${padUrl()}/permanent`,
+      contentType: 'application/json',
+      data: JSON.stringify({
+        export: $('#ep_permanent_exporter-export').is(':checked'),
+        cookie: $('#ep_permanent_exporter-cookie').val(),
+      }),
+      dataType: 'json',
+      // eslint-disable-next-line no-console
+      complete: (jqXHR, textStatus) => console.log('ajax complete', jqXHR, textStatus),
+    });
+  });
 };
 
 export { documentReady };
