e30eca15985017a29fa2af9b4a304ed031368b51
Jason Owen <jason@jasonaowen.net>
2021-04-28 17:19:16 -0700

    Switch UI based on web monetization status
    We want to gate the sync-to-Permanent functionality behind web
    monetization being enabled. First, check to see if the COIL plugin is
    installed; then, wait for the plugin to report that payments are
    flowing.
    
    There is a bit of a race condition we want to avoid here: it could be
    the case that our document ready hook gets called before or after
    payments begin flowing. We need to check the current state at the time
    of hook execution to determine which case we're in, and if payments have
    not yet started, register the callback for when they are.
    
    The types-wm package declares TypeScript types for web monetization.
    Since the web monetization API is through a property on the global
    `document` object, we don't (yet) actually need any of the types it
    declares, but we do need to import the package in order to get access to
    that extended global. We can import an empty list of types in order to
    satisfy both the compiler and the linter.
    
    [1] https://www.npmjs.com/package/types-wm

--- src/client/document-ready.ts
+++ src/client/document-ready.ts
@@ -1,12 +1,41 @@
 import type JQuery from 'jquery';
+import type { } from 'types-wm';
 
 const padUrl = () => (
   window.location.href.replace(window.location.search, '')
 );
 
+const monetizationPending = () => {
+  $('#ep_permanent_exporter-monetization-missing').hide();
+  $('#ep_permanent_exporter-monetization-loading').show();
+  $('#ep_permanent_exporter-monetization-started').hide();
+};
+
+const monetizationStart = () => {
+  $('#ep_permanent_exporter-monetization-missing').hide();
+  $('#ep_permanent_exporter-monetization-loading').hide();
+  $('#ep_permanent_exporter-monetization-started').show();
+};
+
+const initializeMonetization = () => {
+  if (!document.monetization) {
+    return;
+  }
+  monetizationPending();
+  if (document.monetization.state === 'started') {
+    monetizationStart();
+  } else {
+    document.monetization.addEventListener('monetizationstart', monetizationStart);
+  }
+};
+
 const documentReady = (hookName: string) => {
   // eslint-disable-next-line no-console
   console.log('ep_permanent_exporter hook called:', hookName);
+  if (!document.monetization) {
+    return;
+  }
+  initializeMonetization();
 
   $('#ep_permanent_exporter-export-form').submit((event: JQuery.SubmitEvent) => {
     event.preventDefault();
